bf44f4106b4931ab5134249f3c3546b727a69799
MasterFenikkusu <phoenixmusic999@gmail.com>
2018-04-01 21:26:18 +0300

    custom logs

--- RiasBot/Services/LoggingService.cs
+++ RiasBot/Services/LoggingService.cs
@@ -12,18 +12,24 @@ namespace RiasBot.Services
 {
     public class LoggingService : IRService
     {
+        private readonly CommandHandler _ch;
         private readonly DiscordSocketClient _discord;
         private readonly CommandService _commands;
 
         private string _logDirectory = Path.Combine(Environment.CurrentDirectory, "logs");
         private string _logFile => Path.Combine(_logDirectory, $"{DateTime.UtcNow.ToString("yyyy-MM-dd")}.txt");
-        public LoggingService(DiscordSocketClient discord, CommandService commands)
+
+        private bool ready;
+        public LoggingService(CommandHandler ch, DiscordSocketClient discord, CommandService commands)
         {
+            _ch = ch;
             _discord = discord;
             _commands = commands;
-            
+
+            _discord.Ready += Ready;
             _discord.Log += DiscordLogAsync;
             _commands.Log += DiscordLogAsync;
+            _commands.CommandExecuted += CommandLogAsync;
         }
 
         private Task DiscordLogAsync(LogMessage msg)
@@ -33,9 +39,48 @@ namespace RiasBot.Services
             if (!File.Exists(_logFile))               // Create today's log file if it doesn't exist
                 File.Create(_logFile).Dispose();
 
+            var logText = new List<string>();
+
+            if (ready)
+            {
+                if (msg.Severity == LogSeverity.Error || msg.Severity == LogSeverity.Critical)
+                {
+                    logText.Add($"{DateTime.UtcNow.ToString("hh:mm:ss")} [{msg.Severity}] {msg.Source}: {msg.Exception?.ToString() ?? msg.Message}");
+                    try
+                    {
+                        File.AppendAllLinesAsync(_logFile, logText);     // Write the log text to a file
+                    }
+                    catch
+                    {
+
+                    }
+                    return Console.Out.WriteLineAsync(logText[0]);
+                }
+            }
+            else
+            {
+                logText.Add($"{DateTime.UtcNow.ToString("hh:mm:ss")} [{msg.Severity}] {msg.Source}: {msg.Exception?.ToString() ?? msg.Message}");
+                try
+                {
+                    File.AppendAllLinesAsync(_logFile, logText);     // Write the log text to a file
+                }
+                catch
+                {
+
+                }
+                return Console.Out.WriteLineAsync(logText[0]);
+            }
+            return null;
+        }
+
+        private Task CommandLogAsync(CommandInfo commandInfo, ICommandContext context, IResult result)
+        {
             var logText = new List<string>()
             {
-                $"{DateTime.UtcNow.ToString("hh:mm:ss")} [{msg.Severity}] {msg.Source}: {msg.Exception?.ToString() ?? msg.Message}"
+                $"{DateTime.UtcNow.ToString("hh:mm:ss")} [Command] \"{commandInfo.Name}\"",
+                $"\t[User] \"{context.User}\" ({context.User.Id})",
+                $"\t[Channel] \"{context.Channel.Name}\" ({context.Channel.Id})",
+                $"\t[Guild] \"{context.Guild.Name}\" ({context.Guild.Id})"
             };
             try
             {
@@ -45,7 +90,13 @@ namespace RiasBot.Services
             {
 
             }
-            return Console.Out.WriteLineAsync(logText[0]);
+            return Console.Out.WriteLineAsync(String.Join("\n", logText));
+        }
+
+        private Task Ready()
+        {
+            ready = true;
+            return null;
         }
     }
 }
