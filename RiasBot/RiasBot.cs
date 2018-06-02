f0ad4bdb9027827a9c1bf37b32085b50d0625d0f
MasterFenikkusu <phoenixmusic999@gmail.com>
2018-06-02 04:20:40 +0300

    mistakes

--- RiasBot/RiasBot.cs
+++ RiasBot/RiasBot.cs
@@ -19,7 +19,7 @@ namespace RiasBot
         public static void Main(string[] args)
             =>new RiasBot().StartAsync().GetAwaiter().GetResult();
 
-        public static string version = "1.5.7.4";
+        public static string version = "1.5.7.5";
         public static uint goodColor = 0x009688;
         public static uint badColor = 0xff0000;
         public static string currency = "<:heart_diamond:416513090549448724>";
@@ -29,7 +29,7 @@ namespace RiasBot
         public static string creatorServer = "https://discord.gg/VPfBvBt";
         public static ulong supportServer = 416492045859946507;
         public static string patreon = "https://www.patreon.com/riasbot";
-        public static string website = "https://riasbot.me/";
+        public static string website = "https://loveneko.me/";
         public static Stopwatch upTime = new Stopwatch();
         public static int commandsRun = 0;
 
