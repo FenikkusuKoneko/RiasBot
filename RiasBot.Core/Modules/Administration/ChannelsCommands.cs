6842418c9fbbde0a269555c632fdeba77159125e
FenikkusuNeko <fenikkusukoneko@gmail.com>
2018-07-12 04:02:19 +0300

    fixed all issues from the channels commands

--- RiasBot.Core/Modules/Administration/ChannelsCommands.cs
+++ RiasBot.Core/Modules/Administration/ChannelsCommands.cs
@@ -33,7 +33,7 @@ namespace RiasBot.Modules.Administration
             [RequireContext(ContextType.Guild)]
             public async Task CreateChannel(string type, [Remainder]string name)
             {
-                name = name.Replace(" ", "-");
+                type = type.ToLowerInvariant();
                 if (name.Length < 2 || name.Length > 100)
                 {
                     await Context.Channel.SendConfirmationEmbed($"{Context.User.Mention} the name length must be between 2 and 100 characters");
@@ -43,161 +43,177 @@ namespace RiasBot.Modules.Administration
                 {
                     case "text":
                         await Context.Guild.CreateTextChannelAsync(name).ConfigureAwait(false);
-                        await Context.Channel.SendConfirmationEmbed($"Text channel {Format.Bold(name)} was created successfully.").ConfigureAwait(false);
+                        await Context.Channel.SendConfirmationEmbed($"{Context.User.Mention} {type} channel {Format.Bold(name)} was created successfully.").ConfigureAwait(false);
                         break;
                     case "voice":
                         await Context.Guild.CreateVoiceChannelAsync(name).ConfigureAwait(false);
-                        await Context.Channel.SendConfirmationEmbed($"Voice channel {Format.Bold(name)} was created successfully.").ConfigureAwait(false);
+                        await Context.Channel.SendConfirmationEmbed($"{Context.User.Mention} {type} channel {Format.Bold(name)} was created successfully.").ConfigureAwait(false);
                         break;
                 }
             }
 
-            [RiasCommand]
-            [@Alias]
-            [Description]
-            [@Remarks]
+            [RiasCommand][@Alias]
+            [Description][@Remarks]
             [RequireUserPermission(GuildPermission.ManageChannels)]
             [RequireBotPermission(GuildPermission.ManageChannels)]
             [RequireContext(ContextType.Guild)]
             public async Task DeleteChannel(string type, [Remainder]string name)
             {
-                name = name.Replace(" ", "-");
-                if (name.Length < 2 || name.Length > 100)
-                {
-                    await Context.Channel.SendConfirmationEmbed($"{Context.User.Mention} the name length must be between 2 and 100 characters").ConfigureAwait(false);
-                    return;
-                }
+                type = type.ToLowerInvariant();
+
                 IGuildChannel channel = null;
                 switch (type)
                 {
                     case "text":
-                        channel = (await Context.Guild.GetTextChannelsAsync().ConfigureAwait(false)).Where(x => x.Name == name).FirstOrDefault();
+                        name = name.Replace(" ", "-");
+                        channel = (await Context.Guild.GetTextChannelsAsync()).Where(x => x.Name.ToLowerInvariant() == name.ToLowerInvariant()).FirstOrDefault();
                         break;
                     case "voice":
-                        channel = (await Context.Guild.GetVoiceChannelsAsync().ConfigureAwait(false)).Where(x => x.Name == name).FirstOrDefault();
+                        channel = (await Context.Guild.GetVoiceChannelsAsync()).Where(x => x.Name.ToLowerInvariant() == name.ToLowerInvariant()).FirstOrDefault();
                         break;
+                    default:
+                        await Context.Channel.SendErrorEmbed($"{Context.User.Mention} the type of the channel is invalid. Must be text or voice.").ConfigureAwait(false);
+                        return;
                 }
-
                 if (channel != null)
                 {
-                    await channel.DeleteAsync();
-                    await Context.Channel.SendConfirmationEmbed($"Text channel {Format.Bold(name)} was deleted successfully.").ConfigureAwait(false);
+                    var permissions = (await Context.Guild.GetCurrentUserAsync()).GetPermissions(channel);
+                    if (permissions.ViewChannel)
+                    {
+                        if (channel.Id != Context.Channel.Id)
+                        {
+                            await channel.DeleteAsync();
+                            await Context.Channel.SendConfirmationEmbed($"{Context.User.Mention} {type} channel {Format.Bold(name)} was deleted successfully.").ConfigureAwait(false);
+                        }
+                        else
+                        {
+                            await Context.Channel.SendErrorEmbed($"{Context.User.Mention} you can't delete the channel in that this command is executed.").ConfigureAwait(false);
+                        }
+                    }
+                    else
+                    {
+                        await Context.Channel.SendErrorEmbed($"{Context.User.Mention} I don't have the permission to view that channel.");
+                    }
                 }
                 else
                 {
-                    await Context.Channel.SendConfirmationEmbed($"{Context.User.Mention} I couldn't find the channel.").ConfigureAwait(false);
+                    await Context.Channel.SendErrorEmbed($"{Context.User.Mention} I couldn't find the channel.").ConfigureAwait(false);
                 }
             }
 
-            [RiasCommand]
-            [@Alias]
-            [Description]
-            [@Remarks]
+            [RiasCommand][@Alias]
+            [Description][@Remarks]
             [RequireUserPermission(GuildPermission.ManageChannels)]
             [RequireBotPermission(GuildPermission.ManageChannels)]
             [RequireContext(ContextType.Guild)]
             public async Task CreateCategory([Remainder]string name)
             {
-                name = name.ToUpper();
                 if (name.Length < 2 || name.Length > 100)
                 {
                     await Context.Channel.SendConfirmationEmbed($"{Context.User.Mention} the name length must be between 2 and 100 characters");
                     return;
                 }
                 await Context.Guild.CreateCategoryAsync(name).ConfigureAwait(false);
-                await Context.Channel.SendConfirmationEmbed($"Category {Format.Bold(name)} was created successfully.");
+                await Context.Channel.SendConfirmationEmbed($"{Context.User.Mention} category {Format.Bold(name)} was created successfully.");
             }
 
-            [RiasCommand]
-            [@Alias]
-            [Description]
-            [@Remarks]
+            [RiasCommand][@Alias]
+            [Description][@Remarks]
             [RequireUserPermission(GuildPermission.ManageChannels)]
             [RequireBotPermission(GuildPermission.ManageChannels)]
             [RequireContext(ContextType.Guild)]
             public async Task DeleteCategory([Remainder]string name)
             {
-                name = name.ToUpper();
-                if (name.Length < 2 || name.Length > 100)
+                ICategoryChannel category = (await Context.Guild.GetCategoriesAsync().ConfigureAwait(false)).Where(x => x.Name.ToLowerInvariant() == name.ToLowerInvariant()).FirstOrDefault();
+                if (category != null)
                 {
-                    await Context.Channel.SendConfirmationEmbed($"{Context.User.Mention} the name length must be between 2 and 100 characters");
-                    return;
+                    var permissions = (await Context.Guild.GetCurrentUserAsync()).GetPermissions(category);
+                    if (permissions.ViewChannel)
+                    {
+                        await category.DeleteAsync().ConfigureAwait(false);
+                        await Context.Channel.SendConfirmationEmbed($"{Context.User.Mention} category channel {Format.Bold(name)} was deleted successfully.");
+                    }
+                    else
+                    {
+                        await Context.Channel.SendErrorEmbed($"{Context.User.Mention} I don't have the permission to view that category channel.");
+                    }
                 }
-                var category = (await Context.Guild.GetCategoriesAsync().ConfigureAwait(false)).Where(x => x.Name == name).FirstOrDefault();
-                await category.DeleteAsync().ConfigureAwait(false);
-                await Context.Channel.SendConfirmationEmbed($"Category {Format.Bold(name)} was deleted.");
+                else
+                {
+                    await Context.Channel.SendErrorEmbed($"{Context.User.Mention} I couldn't find the category channel.").ConfigureAwait(false);
+                }
             }
 
-            [RiasCommand]
-            [@Alias]
-            [Description]
-            [@Remarks]
+            [RiasCommand][@Alias]
+            [Description][@Remarks]
             [RequireUserPermission(GuildPermission.ManageChannels)]
             [RequireBotPermission(GuildPermission.ManageChannels)]
             [RequireContext(ContextType.Guild)]
             public async Task RenameChannel(string type, [Remainder]string channels)
             {
-                try
+                IGuildChannel channel = null;
+                var chns = channels.Split("->");
+                string oldName = chns[0].TrimEnd();
+                string newName = chns[1].TrimStart();
+                switch (type)
                 {
-                    IGuildChannel channel = null;
-                    var chns = channels.Split("->");
-                    string oldName = chns[0].TrimEnd();
-                    string newName = chns[1].TrimStart();
-
-                    switch (type)
+                    case "text":
+                        oldName = oldName.Replace(" ", "-");
+                        channel = (await Context.Guild.GetTextChannelsAsync().ConfigureAwait(false)).Where(x => x.Name.ToLowerInvariant() == oldName.ToLowerInvariant()).FirstOrDefault();
+                        break;
+                    case "voice":
+                        channel = (await Context.Guild.GetVoiceChannelsAsync().ConfigureAwait(false)).Where(x => x.Name.ToLowerInvariant() == oldName.ToLowerInvariant()).FirstOrDefault();
+                        break;
+                    default:
+                        await Context.Channel.SendErrorEmbed($"{Context.User.Mention} the type of the channel is invalid. Must be text or voice.").ConfigureAwait(false);
+                        return;
+                }
+                if (channel != null)
+                {
+                    var permissions = (await Context.Guild.GetCurrentUserAsync()).GetPermissions(channel);
+                    if (permissions.ViewChannel)
                     {
-                        case "text":
-                            channel = (await Context.Guild.GetTextChannelsAsync().ConfigureAwait(false)).Where(x => x.Name == oldName).FirstOrDefault();
-                            await channel.ModifyAsync(x => x.Name = newName).ConfigureAwait(false);
-                            await Context.Channel.SendConfirmationEmbed($"The name of text channel {Format.Bold(oldName)} was renamed to {Format.Bold(newName)} successfully.").ConfigureAwait(false);
-                            break;
-                        case "voice":
-                            channel = (await Context.Guild.GetVoiceChannelsAsync().ConfigureAwait(false)).Where(x => x.Name == oldName).FirstOrDefault();
-                            await channel.ModifyAsync(x => x.Name = newName).ConfigureAwait(false);
-                            await Context.Channel.SendConfirmationEmbed($"The name of voice channel {Format.Bold(oldName)} was renamed to {Format.Bold(newName)} successfully.").ConfigureAwait(false);
-                            break;
-                        default:
-                            await Context.Channel.SendErrorEmbed($"{Context.User.Mention} I couldn't find the channel.");
-                            break;
+                        await channel.ModifyAsync(x => x.Name = newName).ConfigureAwait(false);
+                        await Context.Channel.SendConfirmationEmbed($"{Context.User.Mention} the name of the {type} channel {Format.Bold(oldName)} was renamed to {Format.Bold(newName)} successfully.").ConfigureAwait(false);
+                    }
+                    else
+                    {
+                        await Context.Channel.SendErrorEmbed($"{Context.User.Mention} I don't have the permission to view that channel.");
                     }
                 }
-                catch
+                else
                 {
-                    await Context.Channel.SendErrorEmbed($"{Context.User.Mention} I couldn't find the channel.");
+                    await Context.Channel.SendErrorEmbed($"{Context.User.Mention} I couldn't find the channel.").ConfigureAwait(false);
                 }
             }
 
-            [RiasCommand]
-            [@Alias]
-            [Description]
-            [@Remarks]
+            [RiasCommand][@Alias]
+            [Description][@Remarks]
             [RequireUserPermission(GuildPermission.ManageChannels)]
             [RequireBotPermission(GuildPermission.ManageChannels)]
             [RequireContext(ContextType.Guild)]
             public async Task RenameCategory([Remainder]string categories)
             {
-                try
+                var cats = categories.Split("->");
+                string oldName = cats[0].TrimEnd();
+                string newName = cats[1].TrimStart();
+                var category = (await Context.Guild.GetCategoriesAsync().ConfigureAwait(false)).Where(x => x.Name.ToLowerInvariant() == oldName.ToLowerInvariant()).FirstOrDefault();
+                if (category != null)
                 {
-                    ICategoryChannel category = null;
-                    var cats = categories.Split("->");
-                    string oldName = cats[0].TrimEnd().ToUpper();
-                    string newName = cats[1].TrimStart().ToUpper();
-
-                    category = (await Context.Guild.GetCategoriesAsync().ConfigureAwait(false)).Where(x => x.Name.ToUpper() == oldName).FirstOrDefault();
-
-                    if (category != null)
+                    var permissions = (await Context.Guild.GetCurrentUserAsync()).GetPermissions(category);
+                    if (permissions.ViewChannel)
                     {
                         await category.ModifyAsync(x => x.Name = newName).ConfigureAwait(false);
-                        await Context.Channel.SendConfirmationEmbed($"The name of category {Format.Bold(oldName)} was renamed to {Format.Bold(newName)} successfully.").ConfigureAwait(false);
+                        await Context.Channel.SendConfirmationEmbed($"{Context.User.Mention} the name of the category channel {Format.Bold(oldName)} was renamed to {Format.Bold(newName)} successfully.").ConfigureAwait(false);
                     }
                     else
                     {
-                        await Context.Channel.SendErrorEmbed($"{Context.User.Mention} I couldn't find the category.").ConfigureAwait(false);
+                        await Context.Channel.SendErrorEmbed($"{Context.User.Mention} I don't have the permission to view that category channel.");
                     }
                 }
-                catch
+                else
                 {
-                    await Context.Channel.SendErrorEmbed($"{Context.User.Mention} I couldn't find the category.");
+                    await Context.Channel.SendErrorEmbed($"{Context.User.Mention} I couldn't find the category.").ConfigureAwait(false);
                 }
             }
 
@@ -235,20 +251,16 @@ namespace RiasBot.Modules.Administration
                 }
             }
 
-            [RiasCommand]
-            [@Alias]
-            [Description]
-            [@Remarks]
+            [RiasCommand][@Alias]
+            [Description][@Remarks]
             [RequireUserPermission(GuildPermission.ManageChannels)]
             [RequireBotPermission(GuildPermission.ManageChannels)]
             [RequireContext(ContextType.Guild)]
             public async Task SetNSFWChannel(ITextChannel channel = null)
             {
-                bool thisChannel = false;
                 if (channel is null)
                 {
                     channel = (ITextChannel)Context.Channel;
-                    thisChannel = true;
                 }
                 var permissions = (await Context.Guild.GetCurrentUserAsync()).GetPermissions(channel);
                 if (permissions.ViewChannel)
@@ -256,7 +268,7 @@ namespace RiasBot.Modules.Administration
                     if (channel.IsNsfw)
                     {
                         await channel.ModifyAsync(x => x.IsNsfw = false).ConfigureAwait(false);
-                        if (thisChannel)
+                        if (channel.Id == Context.Channel.Id)
                             await Context.Channel.SendConfirmationEmbed($"{Context.User.Mention} this channel is not NSFW anymore.");
                         else
                             await Context.Channel.SendConfirmationEmbed($"{Context.User.Mention} {Format.Bold(channel.Name)} is not NSFW anymore.");
@@ -264,7 +276,7 @@ namespace RiasBot.Modules.Administration
                     else
                     {
                         await channel.ModifyAsync(x => x.IsNsfw = true).ConfigureAwait(false);
-                        if (thisChannel)
+                        if (channel.Id == Context.Channel.Id)
                             await Context.Channel.SendConfirmationEmbed($"{Context.User.Mention} this channel is now NSFW.");
                         else
                             await Context.Channel.SendConfirmationEmbed($"{Context.User.Mention} {Format.Bold(channel.Name)} is now NSFW.");
