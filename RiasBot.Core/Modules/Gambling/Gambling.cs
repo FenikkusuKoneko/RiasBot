55caab0a873efbed0f6aa1c42f4a545e33a612b4
FenikkusuNeko <fenikkusukoneko@gmail.com>
2018-10-04 17:41:53 +0300

    put blackjack in the main Gambling Module

--- RiasBot.Core/Modules/Gambling/Gambling.cs
+++ RiasBot.Core/Modules/Gambling/Gambling.cs
@@ -9,20 +9,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using RiasBot.Extensions;
+using RiasBot.Modules.Gambling.Services;
 
 namespace RiasBot.Modules.Gambling
 {
     public partial class Gambling : RiasModule
     {
         private readonly CommandHandler _ch;
-        private readonly CommandService _service;
         private readonly DbService _db;
+        private readonly BlackjackService _blackjackService;
 
-        public Gambling(CommandHandler ch, CommandService service, DbService db)
+        public Gambling(CommandHandler ch, DbService db, BlackjackService blackjackService)
         {
             _ch = ch;
-            _service = service;
             _db = db;
+            _blackjackService = blackjackService;
         }
 
         [RiasCommand][@Alias]
@@ -72,6 +73,69 @@ namespace RiasBot.Modules.Gambling
                         await Context.Channel.SendErrorMessageAsync($"You don't have enough {RiasBot.Currency}.");
                     }
                 }
+            }
+        }
+        
+        [RiasCommand][@Alias]
+        [Description][@Remarks]
+        [RequireContext(ContextType.Guild)]
+        [Priority(1)]
+        public async Task BlackjackAsync(int bet)
+        {
+            var currency = _blackjackService.GetCurrency((IGuildUser)Context.User);
+            if (bet < 5)
+            {
+                await Context.Channel.SendErrorMessageAsync($"You cannot bet less than 5 {RiasBot.Currency}.").ConfigureAwait(false);
+            }
+            else if (bet > 1000)
+            {
+                await Context.Channel.SendErrorMessageAsync($"You cannot bet more than 1000 {RiasBot.Currency}.").ConfigureAwait(false);
+            }
+            else if (bet <= currency)
+            {
+                var bj = _blackjackService.GetGame((IGuildUser) Context.User);
+                if (bj is null)
+                {
+                    bj = _blackjackService.GetOrCreateGame((IGuildUser) Context.User);
+                    await bj.InitializeGameAsync(Context.Channel, (IGuildUser)Context.User, bet).ConfigureAwait(false); 
+                }
+                else
+                {
+                    await Context.Channel.SendErrorMessageAsync($"You are already in a blackjack session! Type `{_ch.Prefix}bj resume` to continue the session in this channel.").ConfigureAwait(false);
+                } 
+            }
+            else
+            {
+                await Context.Channel.SendErrorMessageAsync($"You don't have enough {RiasBot.Currency}.").ConfigureAwait(false);
+            }
+        }
+        
+        [RiasCommand][@Alias]
+        [Description][@Remarks]
+        [RequireContext(ContextType.Guild)]
+        [Priority(0)]
+        public async Task BlackjackAsync(string subcommand)
+        {
+            subcommand = subcommand.ToLowerInvariant();
+            var bj = _blackjackService.GetGame((IGuildUser) Context.User);
+            switch (subcommand)
+            {
+                case "resume":
+                    if (bj != null)
+                        await bj.ResumeGameAsync((IGuildUser) Context.User, Context.Channel).ConfigureAwait(false);
+                    else
+                        await Context.Channel.SendErrorMessageAsync("You are not in a blackjack session!").ConfigureAwait(false);
+                    break;
+                case "stop":
+                case "surrender":
+                    if (bj != null)
+                    {
+                        await bj.StopGameAsync((IGuildUser)Context.User).ConfigureAwait(false);
+                        await Context.Channel.SendConfirmationMessageAsync("Blackjack stopped!").ConfigureAwait(false);
+                    }
+                    else
+                        await Context.Channel.SendErrorMessageAsync("You are not in a blackjack session!").ConfigureAwait(false);
+                    break;
             }
         }
     }
