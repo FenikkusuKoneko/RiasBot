b1f7f8ef67e1e93e3bd458ef7919707875ebd99d
FenikkusuNeko <fenikkusukoneko@gmail.com>
2018-11-16 16:57:53 +0200

    edited the RateLimit for the ping command, stop spamming it

--- RiasBot.Core/Modules/Utility/Utility.cs
+++ RiasBot.Core/Modules/Utility/Utility.cs
@@ -88,7 +88,7 @@ namespace RiasBot.Modules.Utility
 
         [RiasCommand][@Alias]
         [Description][@Remarks]
-        [Ratelimit(3, 5, Measure.Seconds, applyPerGuild: true)]
+        [Ratelimit(1, 5, Measure.Seconds, applyPerGuild: true)]
         public async Task Ping()
         {
             var stopwatch = new Stopwatch();
