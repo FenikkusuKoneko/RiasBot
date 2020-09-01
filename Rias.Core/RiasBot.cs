f33a5466fd1a8deb9fc4fc262292ce7498199883
FenikkusuKoneko <fenikkusukoneko@gmail.com>
2020-09-01 11:51:42 +0300

    Version up

--- Rias.Core/RiasBot.cs
+++ Rias.Core/RiasBot.cs
@@ -29,7 +29,7 @@ namespace Rias.Core
     public class RiasBot : IServiceProvider
     {
         public const string Author = "Koneko#0001";
-        public const string Version = "3.2.2";
+        public const string Version = "3.2.3";
         public static readonly Stopwatch UpTime = new Stopwatch();
 
         public readonly DiscordShardedClient Client;
