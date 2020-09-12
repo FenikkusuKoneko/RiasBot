a3f8cdac02a409500db61ce3b13aa1007ea15493
FenikkusuKoneko <fenikkusukoneko@gmail.com>
2020-09-12 22:25:58 +0300

    Fixed the DateAdded column in the database

--- Rias/Database/Entities/DbEntity.cs
+++ Rias/Database/Entities/DbEntity.cs
@@ -8,6 +8,6 @@ namespace Rias.Database.Entities
         [Key]
         public int Id { get; set; }
         
-        public DateTime DateAdded { get; } = DateTime.UtcNow;
+        public DateTime DateAdded { get; set; } = DateTime.UtcNow;
     }
 }
